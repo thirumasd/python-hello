cf0c9b1d7d765f264c8a6640c543485fb5185bda
thirumasd <113158761+thirumasd@users.noreply.github.com>
2022-09-14 16:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World - Python app')
     return 'Hello {}!\n'.format(target)
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
